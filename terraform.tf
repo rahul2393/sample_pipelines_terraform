5f07af09ddb3d6d7086c3361638fc8e47a8e03d6
Rahul Yadav <rahul.yadav@hotcocoasoftware.com>
2018-08-30 11:57:17 +0530

    add README

--- terraform.tf
+++ terraform.tf
@@ -4,14 +4,14 @@ provider "aws" {}
 
 # EC2 instances
 resource "aws_instance" "sampleInstances" {
-  count = 2
+  count = 1
 
   # ami = "${var.ecsAmi}"
-  ami = "ami-04169656fea786776"
+  ami = "ami-00b7e666605d33085"
   #The AMI specified above is a public Ubuntu image, backed by EBS. It was available on AWS at the time of
   #authoring this sample. You can also specify your own AMI image or use any other public EBS backed image if the
   #AMI used in the sample is decommissioned.
-  availability_zone = "us-east-1b"   #REPLACE THIS WITH YOUR AVAILABILITY ZONE
+  availability_zone = "ap-south-1"   #REPLACE THIS WITH YOUR AVAILABILITY ZONE
   instance_type = "t2.micro"
   associate_public_ip_address = true
   source_dest_check = false
